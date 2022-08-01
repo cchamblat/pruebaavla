463ad4a1204386c2f95d25053302fbbe2812be5b
Christian Chamblat <christian.chamblat@u-payments.com>
2022-08-01 16:35:27 -0400

    First commit

---
+++ src/main/java/com/backend/pruebaavla/PruebaavlaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pruebaavla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaavlaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaavlaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/pruebaavla/PruebaavlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pruebaavla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaavlaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
